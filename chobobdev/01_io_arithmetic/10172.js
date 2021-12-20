213952a2fadde1e31c42320fecd5c607a06fd093
chobobdev <chobobdev@naver.com>
2021-12-20 22:14:00 +0900

    [Solved] All of 01_IO_Arithmetic

---
+++ chobobdev/01_io_arithmetic/10172.js
@@ -0,0 +1,4 @@
+let dogs=["|\\_/|", "|q p|   /}", "( 0 )\"\"\"\\", "|\"^\"`    |","||_/=\\\\__|"]
+for (let i = 0; i < dogs.length; i++) {
+    console.log(dogs[i])
+}
---
+++ chobobdev/01_io_arithmetic/10869.py
@@ -0,0 +1,8 @@
+inputs=input().split(" ")
+a = int(inputs[0])
+b = int(inputs[1])
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a%b)
