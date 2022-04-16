719902ce60d7c2e87a0822c2ea828c6dcc4c22dd
project-mizzu <project.mizzu@gmail.com>
2022-04-17 02:04:15 +0900

    chapter.03 ~06

---
+++ project-mizzu/03_loop/06_2742.js
@@ -0,0 +1,14 @@
+// [문제] 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+// [입력] 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+// [출력] 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+
+// [정답]
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+let N = parseInt(input);
+let answer = "";
+
+for ( let i = N; i >= 1; i-- ) {
+  answer += i + "\n";
+}
+
+console.log(answer);
