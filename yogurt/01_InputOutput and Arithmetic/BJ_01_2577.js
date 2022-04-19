8b3dfc2762693328d5e62256e60f037dac498135
Minju Park <project.mizzu@gmail.com>
2022-04-19 23:40:07 +0900

    [yogurt-220419] (#63)
    Co-authored-by: userHWSeo <gmldnjseocnd@naver.com>

---
+++ yogurt/01_InputOutput and Arithmetic/BJ_01_2577.js
@@ -0,0 +1,7 @@
+// 문제 : Hello World!를 출력하시오.
+// 입력 : 없음
+// 출력 : Hello World!
+
+console.log("Hello World!");
+
+// console.log() 를 활용한 문제
---
+++ yogurt/01_InputOutput and Arithmetic/BJ_06_1001.js
@@ -0,0 +1,13 @@
+// 문제 : 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력 : 첫째 줄에 A-B를 출력한다.
+
+const fs = require("fs");
+const inputData = fs.readFileSync(0, "utf8").toString().split(" ");
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A - B);
+
+// 입력값을 배열로 받아 배열의 [0] - [1]을 해준다.
